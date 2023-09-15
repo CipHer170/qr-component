1d42f72405101c9d5c645439db53beed9037d526
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2023-09-15 15:56:19 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import BaseContainer from "./Components/BaseContainer/BaseContainer";
+import image from "./images/image-qr-code.png";
+import "./App.scss";
 function App() {
   return (
-    <div className="App">
-      <BaseContainer>qr-component</BaseContainer>
-    </div>
+    <BaseContainer>
+      <div className="qr">
+        <img src={image} alt="qr-code" className="qr__code" />
+        <div className="qr__info">
+          <h3>Improve your front-end skills by building projects</h3>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
+    </BaseContainer>
   );
 }
 
