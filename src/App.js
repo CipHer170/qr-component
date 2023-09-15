63924ea44ad7b40a18f9b1ed48c33e3d7c0eb8ca
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2023-09-15 14:55:56 +0500

    linked base container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import BaseContainer from "./Components/BaseContainer/BaseContainer";
 function App() {
-  return <div className="App">qr-component</div>;
+  return (
+    <div className="App">
+      <BaseContainer>qr-component</BaseContainer>
+    </div>
+  );
 }
 
 export default App;
